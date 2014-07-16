88c3672f279b5c8c4f4dffe817e7c57fa12956d5
Kriol <kriolitr@gmail.com>
2014-07-16 21:36:50 +0400

    update

--- main.py
+++ main.py
@@ -3,8 +3,24 @@
 
 import re
 
-RE = re.compile(r'<[tT][dD]>\d?\d.\d?\d.(\d\d)?\d\d.')
+RE = re.compile(
+    r'''
+        <[tT][dD]>
+            (\d?\d)\.(\d?\d)\.((?:\d\d)?\d\d)
+        </[tT][dD]><[tT][dD]>
+            (\d+)
+        </[tT][dD]><[tT][dD]>
+            (\d+)[,\.](\d\d\d\d)
+        </[tT][dD]>
+    ''',
+    flags=re.VERBOSE)
 
 with open('data.html', 'rt', encoding='utf-8') as src:
     for line in src:
-        print(line)
+        M = RE.search(line)
+        if M:
+            day, mon, yea, count, rou, kop = M.groups()
+            Date = None
+            Count = int(count)
+            Course = None
+            print(Date, Count, Course)
