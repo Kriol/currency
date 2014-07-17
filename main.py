ae16e4a5a96cd9f51b45848cfe07844926add6ff
Kriol <kriolitr@gmail.com>
2014-07-17 09:01:40 +0400

    project is not completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from datetime import date
 
+import Money
 import re
 
 RE = re.compile(
@@ -15,12 +17,14 @@ RE = re.compile(
     ''',
     flags=re.VERBOSE)
 
+
+
 with open('data.html', 'rt', encoding='utf-8') as src:
     for line in src:
         M = RE.search(line)
         if M:
             day, mon, yea, count, rou, kop = M.groups()
-            Date = None
+            Date = date(int(yea), int(mon), int(day)).strftime("%d/%m/%Y")
             Count = int(count)
-            Course = None
+            Course = Money(rou, kop)
             print(Date, Count, Course)
